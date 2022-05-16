f4b1374a05767d1cee0ac1817a1a1ad1327bb48e
Natalya <trotsyuknata@yandex.ru>
2022-05-16 22:41:27 +0300

    homework

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,18 @@
 package reverse_string
-
+//package main
+import ("fmt")
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	s := []rune(input)
+	r := []rune(input)
+	for i:=0; i<len(r);i++ {
+		r[len(r)-i-1] = s[i]
+	}
+	return string(r)
 }
+
+//func main() {
+//	s := "Jetbrains系"
+//	r:= ReverseString(s)
+//	fmt.Println(r)
+//}
